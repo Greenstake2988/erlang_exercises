f5544f53f9e4643b093752419451f27082aff85f
Oscar Daniel Vazquez Alcocer <oscar.va@valladolid.tecnm.mx>
2023-11-06 17:05:06 -0600

    Kill signals

--- kitchen.erl
+++ kitchen.erl
@@ -36,6 +36,20 @@ store(Pid, Food) ->
     {Pid, Msg} -> Msg
   end.
 
+store2(Pid, Food) ->
+  Pid ! {self(), {store, Food}},
+  receive
+    {Pid, Msg} -> Msg
+  after 3000 -> timeout
+  end.
+
+take2(Pid, Food) ->
+  Pid ! {self(), {take, Food}},
+  receive
+    {Pid, Msg} -> Msg
+  after 3000 -> timeout
+  end.
+
 take(Pid, Food) ->
   Pid ! {self(), {take, Food}},
   receive
